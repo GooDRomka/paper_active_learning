ea4086e79a3cb4091da9d65b86df1383c3eed43f
kozhevnikov.ri <kozhevnikov.ri@phystech.edu>
2022-01-11 14:01:12 +0300

    дебажим 6

--- run_active.py
+++ run_active.py
@@ -251,10 +251,10 @@ elif exp_type == 8:
 
 elif exp_type == 9:
     # self learning rand
-    params = [[STRATEGY.SELF, STRATEGY.LAZY, 800, 8000, 0, 13.5],
+    params = [[STRATEGY.SELF, STRATEGY.LAZY, 3000, 8000, 0, 13.5],
           [STRATEGY.SELF, STRATEGY.LAZY, 1000, 8000,  0, 13.5],
           [STRATEGY.SELF, STRATEGY.LAZY, 2000, 8000,  0, 13.5],
-          [STRATEGY.SELF, STRATEGY.LAZY, 3000, 8000,  0, 13.5],
+          [STRATEGY.SELF, STRATEGY.LAZY, 800, 8000,  0, 13.5],
           [STRATEGY.SELF, STRATEGY.LAZY, 2400, 8000,  0, 13.5],
           [STRATEGY.SELF, STRATEGY.LAZY, 4000, 8000,  0, 13.5],
           [STRATEGY.SELF, STRATEGY.LAZY, 1200, 8000,  0, 13.5],
