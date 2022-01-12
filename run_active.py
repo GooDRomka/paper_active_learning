723250650700732fd5119ded4bede1985bc56c27
kozhevnikov.ri <kozhevnikov.ri@phystech.edu>
2022-01-12 16:44:36 +0300

    вернул норм параметры

--- run_active.py
+++ run_active.py
@@ -252,10 +252,10 @@ elif exp_type == 8:
 elif exp_type == 9:
     # self learning rand
     params = [
-          [STRATEGY.SELF, STRATEGY.LAZY, 2000, 8000,  0, 0],
-          [STRATEGY.SELF, STRATEGY.LAZY, 2400, 8000,  0, 0],
-          [STRATEGY.SELF, STRATEGY.LAZY, 4000, 8000,  0, 0],
-          [STRATEGY.SELF, STRATEGY.LAZY, 1200, 8000,  0, 0],
+          [STRATEGY.SELF, STRATEGY.LAZY, 2000, 8000,  0, 0.9975],
+          [STRATEGY.SELF, STRATEGY.LAZY, 2400, 8000,  0, 0.9975],
+          [STRATEGY.SELF, STRATEGY.LAZY, 4000, 8000,  0, 0.9975],
+          [STRATEGY.SELF, STRATEGY.LAZY, 1200, 8000,  0, 0.9975],
           ]
     for i in range(5):
         for param in params:
